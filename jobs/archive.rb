4e25fd89c169166f293a4d1c2a06d9302c6becc7
Erik Ordway <ordwaye@evergreen.edu>
2016-07-25 14:46:03 -0700

    Move monkey patch inside Only run once a year run archived in batches.

--- jobs/archive.rb
+++ jobs/archive.rb
@@ -1,15 +1,30 @@
-module Jobs
+#monkey patch Topic to add a useful scope
+class Topic
+  scope :created_before, lambda { |time_ago| where('topics.created_at < ?', time_ago) }
+end
 
+
+module Jobs
   class Archive < ::Jobs::Scheduled
     every 1.day
 
     def execute(args)
       Logster.logger.info "executing AutoArchive"
-
-      if SiteSetting.aa_auto_archived_enabled
-
+      #aer we enabled
+      #is this the year day to run on?
+      if SiteSetting.aa_auto_archived_enabled  && (Time.now.yday ==  SiteSetting.aa_archive_day)
+        #This is horrible but works
+        older_than = Date.new(Time.now.year,1,1) + SiteSetting.aa_archive_older_than_day
+        #use system to archive topics
+        user = User.find_by_username('system')
+        #find the topics to close.
+        #do them in batches for memory performance
+        Topic.created_before(older_than).in_batches do |t|
+          topics.each do |t|
+             t.update_status('archived', true, user)
+          end
+        end
       end
     end
-
   end
 end
