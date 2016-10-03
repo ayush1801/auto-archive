8dc5af9a22ae2c61f79c3b5cac5d67de04f08971
Erik Ordway <ordwaye@evergreen.edu>
2016-10-03 14:55:59 -0700

    Adjust so that it only effects normal Topics and not messages.

--- jobs/archive.rb
+++ jobs/archive.rb
@@ -12,17 +12,19 @@ module Jobs
       Logster.logger.info "executing AutoArchive"
       #aer we enabled
       #is this the year day to run on?
-      if SiteSetting.aa_auto_archived_enabled  && (Time.now.yday ==  SiteSetting.aa_archive_day)
+      if SiteSetting.aa_auto_archived_enabled && (Time.now.yday == SiteSetting.aa_archive_day)
         #This is horrible but works
-        older_than = Date.new(Time.now.year,1,1) + SiteSetting.aa_archive_older_than_day
+        older_than = Date.new(Time.now.year, 1, 1) + SiteSetting.aa_archive_older_than_day
         #use system to archive topics
         user = User.find_by_username('system')
         #find the topics to close.
         #do them in batches for memory performance
-        Topic.created_before(older_than).find_each do |t|
-            #puts "that was created #{t.created_at} going to archive #{t.title} "
-          t.update_status('archived', true, user)
+        Topic.created_before(older_than).find_each(batch_size: 300) do |t|
+          #puts "that was created #{t.created_at} going to archive #{t.title} "
+          if t.archetype == Archetype.default  #only effect not (banner, private_messages)
+            t.update_status('archived', true, user)
           end
+        end
       end
     end
   end
